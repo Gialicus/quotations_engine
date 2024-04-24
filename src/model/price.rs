5f18732a14502f1e81ac35915926ccff837c47fa
g.coletta <giali.coletta@fullstackagency.it>
2024-04-24 13:53:12 +0200

    fix(rule)

--- src/model/price.rs
+++ src/model/price.rs
@@ -1,10 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-use crate::rules::rule::ProductRuleType;
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Price {
     pub price: f64,
     pub discount: f64,
-    pub rules: Vec<ProductRuleType>,
 }
